55d8c4c324448a24e297dc9b1f3bf6b4574692a0
itpstudent <itpstudent@10-17-249-167.dynapool.wireless.nyu.edu>
2019-04-25 22:25:32 -0400

    loop is working

---
+++ rubeserver_banana/arduino_secrets.h
@@ -0,0 +1,11 @@
+#define SECRET_SSID "itpsandbox"// your network SSID (name)
+#define SECRET_PASS "NYU+s0a!+P?"// your network password
+#define SECRET_PATH "POST /rube2019"// verb and path
+#define SECRET_BODY "Rube Roolz"// string for the body of the request
+
+//fill this page
+//upload code with right UTC time for test 
+//check if servo moves
+//reset time to 21 30 UTC 
+//test go pro and other cameras 
+//
